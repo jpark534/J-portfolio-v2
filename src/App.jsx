48a9182d6e975f60992b6f9ae7f13dcc1c0db58b
Julia Park <155590082+jpark534@users.noreply.github.com>
2024-12-09 23:05:54 -0500

    NavBar
    Setting up the Navbar section of the Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div className={styles.App}>
-      Hello World
+      <Navbar />
     </div>
   )
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from"./Navbar.module.css";
+
+export const Navbar = () => {
+  retun (
+    <nav className={styles.navbar}>
+      <a className={styles.title} href= "/">
+        Portfolio
+      </a> 
+      <div className={styles.menu}>
+        <ul className={styles.menuItems}>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#Contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
